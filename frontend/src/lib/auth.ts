cbd6fbeaf566a2854485cf59972b1f2aac158383
Michał <creeperasek@gmail.com>
2026-03-30 00:48:31 +0200

    feat: reorder project + start with auth and session

---
+++ frontend/src/lib/auth.ts
@@ -0,0 +1,16 @@
+import { cookies } from 'next/headers';
+import { encrypt } from './session';
+
+export async function createSession(userId: string) {
+    const expires = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000); // 7 days
+    const session = await encrypt({ userId, expires });
+
+    const cookieStore = await cookies();
+    cookieStore.set('session', session, {
+        httpOnly: true,
+        secure: false,
+        sameSite: 'lax',
+        path: '/',
+        expires,
+    });
+}
